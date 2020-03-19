36c809c762baffad3bfe49d87c9d4c3ca59cf46f
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:48:23 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,8 +3,8 @@ require_relative "../lib/student.rb"
 require 'nokogiri'
 require 'colorize'
 
-class CommandLineInteface
-  BASE_PATH = "./fixtures/student-site/"
+class CommandLineInterface
+  BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
 
   def run
     make_students
@@ -19,7 +19,7 @@ class CommandLineInteface
 
   def add_attributes_to_students
     Student.all.each do |student|
-      attributes = Scraper.scrape_profile_page(student.profile_url)
+      attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
       student.add_student_attributes(attributes)
     end
   end
@@ -39,45 +39,3 @@ class CommandLineInteface
   end
 
 end
-
-# require_relative "../lib/scraper.rb"
-# require_relative "../lib/student.rb"
-# require 'nokogiri'
-# require 'colorize'
-#
-# class CommandLineInterface
-#   BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
-#
-#   def run
-#     make_students
-#     add_attributes_to_students
-#     display_students
-#   end
-#
-#   def make_students
-#     students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
-#     Student.create_from_collection(students_array)
-#   end
-#
-#   def add_attributes_to_students
-#     Student.all.each do |student|
-#       attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
-#       student.add_student_attributes(attributes)
-#     end
-#   end
-#
-#   def display_students
-#     Student.all.each do |student|
-#       puts "#{student.name.upcase}".colorize(:blue)
-#       puts "  location:".colorize(:light_blue) + " #{student.location}"
-#       puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
-#       puts "  bio:".colorize(:light_blue) + " #{student.bio}"
-#       puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
-#       puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
-#       puts "  github:".colorize(:light_blue) + " #{student.github}"
-#       puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-#       puts "----------------------".colorize(:green)
-#     end
-#   end
-#
-# end
