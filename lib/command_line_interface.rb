e8a0bc8c2c21f0d34be63a87156c1cdca930735c
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:43:42 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -44,28 +44,28 @@ end
 # require_relative "../lib/student.rb"
 # require 'nokogiri'
 # require 'colorize'
-# 
+#
 # class CommandLineInterface
 #   BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
-# 
+#
 #   def run
 #     make_students
 #     add_attributes_to_students
 #     display_students
 #   end
-# 
+#
 #   def make_students
 #     students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
 #     Student.create_from_collection(students_array)
 #   end
-# 
+#
 #   def add_attributes_to_students
 #     Student.all.each do |student|
 #       attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
 #       student.add_student_attributes(attributes)
 #     end
 #   end
-# 
+#
 #   def display_students
 #     Student.all.each do |student|
 #       puts "#{student.name.upcase}".colorize(:blue)
@@ -79,5 +79,5 @@ end
 #       puts "----------------------".colorize(:green)
 #     end
 #   end
-# 
+#
 # end
